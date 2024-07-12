824fe651938b8cf7eaf99fac3d3839966a4bf738
minsky922 <minsky922@gmail.com>
2024-07-12 22:17:33 +0900

    f

--- fs/zbd_zenfs.cc
+++ fs/zbd_zenfs.cc
@@ -282,10 +282,11 @@ IOStatus ZonedBlockDevice::Open(bool readonly, bool exclusive) {
   active_io_zones_ = 0;
   open_io_zones_ = 0;
   // uint64_t device_io_capacity = 85899345920;  // 80GB
-  // for (; i < zone_rep->ZoneCount() &&
-  //        (io_zones.size() * zbd_be_->GetZoneSize()) < (device_io_capacity);
-  //      i++) {
-  for (; i < zone_rep->ZoneCount(); i++) {
+  uint64_t device_io_capacity = 32212254720;  // 30GB
+  for (; i < zone_rep->ZoneCount() &&
+         (io_zones.size() * zbd_be_->GetZoneSize()) < (device_io_capacity);
+       i++) {
+    // for (; i < zone_rep->ZoneCount(); i++) {
     /* Only use sequential write required zones */
     if (zbd_be_->ZoneIsSwr(zone_rep, i)) {
       if (!zbd_be_->ZoneIsOffline(zone_rep, i)) {
